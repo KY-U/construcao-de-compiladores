82883208c153f7b4968ce6f392e117582b4f89e8
KY-U <uscaio@estudante.ufscar>
2024-07-16 08:48:16 -0300

    add: tabela de símbolos

---
+++ T3/expr-parser/src/main/java/br/ufscar/dc/compiladores/expr/parser/TabelaDeSimbolos.java
@@ -0,0 +1,40 @@
+package br.ufscar.dc.compiladores.expr.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TabelaDeSimbolos {
+    public enum TipoAlguma {
+        INTEIRO,
+        REAL,
+        INVALIDO
+    }
+    
+    class EntradaTabelaDeSimbolos {
+        String nome;
+        TipoAlguma tipo;
+
+        private EntradaTabelaDeSimbolos(String nome, TipoAlguma tipo) {
+            this.nome = nome;
+            this.tipo = tipo;
+        }
+    }
+    
+    private final Map<String, EntradaTabelaDeSimbolos> tabela;
+    
+    public TabelaDeSimbolos() {
+        this.tabela = new HashMap<>();
+    }
+    
+    public void adicionar(String nome, TipoAlguma tipo) {
+        tabela.put(nome, new EntradaTabelaDeSimbolos(nome, tipo));
+    }
+    
+    public boolean existe(String nome) {
+        return tabela.containsKey(nome);
+    }
+    
+    public TipoAlguma verificar(String nome) {
+        return tabela.get(nome).tipo;
+    }
+}
